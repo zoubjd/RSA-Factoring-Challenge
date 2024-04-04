dc42b445e867795850075aa6b7862ed5e03878be
zoubjd <zouhairbajdouri19@gmail.com>
2024-04-04 03:53:38 +0000

    python

--- factors.py
+++ factors.py
@@ -7,19 +7,19 @@ def factoriser(num):
             return (num//i, i)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: factors <file>")
-        exit()
-    factors = sys.argv[1]
-    try:
-        with open(factors, 'r') as f:
-            lines = f.readlines()
-    except FileNotFoundError:
-        print("File not found")
-        exit()
+
+if len(sys.argv) != 2:
+    print("Usage: factors <file>")
+    exit()
+factors = sys.argv[1]
+try:
+    with open(factors, 'r') as f:
+        lines = f.readlines()
+except FileNotFoundError:
+    print("File not found")
+    exit()
     
-    for line in lines:
-        num = int(line.strip())
-        result = factoriser(num)
-        print("{}={}*{}".format(num, result[0], result[1]))
+for line in lines:
+    num = int(line.strip())
+    result = factoriser(num)
+    print("{}={}*{}".format(num, result[0], result[1]))
